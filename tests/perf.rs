a42dbb0b9865fc1046195f6bc196915d652c1e18
thomj130 <thomj130@my.erau.edu>
2024-10-24 12:38:46 -0400

    Used batches of median values for perf test

--- tests/perf.rs
+++ tests/perf.rs
@@ -7,9 +7,24 @@ use sembas::{
     structs::{Halfspace, WithinMode},
 };
 
+fn get_median(values: &mut Vec<u128>) -> u128 {
+    let length = values.len();
+    if length == 0 {
+        panic!("Cannot find median of empty vec");
+    }
+
+    values.sort();
+    if (length % 2) == 0 {
+        (values[length / 2] + values[length / 2 + 1]) / 2
+    } else {
+        values[length / 2]
+    }
+}
+
 #[test]
 fn measure_sphere_expl_times() {
-    const ITER_COUNT: u32 = 20;
+    const BATCH_COUNT: u32 = 20;
+    const ITER_COUNT: u32 = 100;
 
     let mut sphere = Sphere::new(SVector::repeat(0.5), 0.5, None);
 
@@ -21,17 +36,21 @@ fn measure_sphere_expl_times() {
 
     let mut times = vec![];
 
-    for _ in 0..ITER_COUNT {
-        let adhf = ConstantAdhererFactory::new(angle, None);
-        let mut expl = MeshExplorer::new(d, root, d * 0.9, adhf);
+    for _ in 0..BATCH_COUNT {
+        let mut batch = vec![];
+        for _ in 0..ITER_COUNT {
+            let adhf = ConstantAdhererFactory::new(angle, None);
+            let mut expl = MeshExplorer::new(d, root, d * 0.9, adhf);
 
-        let start_time = Instant::now();
+            let start_time = Instant::now();
 
-        while let Ok(Some(_)) = expl.step(&mut sphere) {}
+            while let Ok(Some(_)) = expl.step(&mut sphere) {}
 
-        times.push((Instant::now() - start_time).as_micros());
+            batch.push((Instant::now() - start_time).as_micros());
+        }
+
+        times.push(get_median(&mut batch));
     }
-
     println!(
         "Mean time: {}us. Max time: {}us.",
         times.iter().sum::<u128>() as f64 / times.len() as f64,
